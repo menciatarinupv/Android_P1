7a87819571c85bc1ed1e5ae86d88ae4d6a01f58c
menciatarinupv <menciatarinupv@gmail.com>
2023-03-14 23:52:20 +0100

    log puesto

--- app/src/main/java/com/example/p_1/MainActivity.java
+++ app/src/main/java/com/example/p_1/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.p_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("prueba", "estoy en oncreate()"); //debug
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
